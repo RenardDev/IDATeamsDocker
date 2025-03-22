621aa7626bafee4ffe3debd47989af9586ac9c98
Ren <zeze839@gmail.com>
2025-03-22 14:11:05 +0300

    IDA 9.1 support

--- hexvault/image/patch_version_mismatch.py
+++ hexvault/image/patch_version_mismatch.py
@@ -2,75 +2,87 @@
 from pathlib import Path
 from sigmak_search import ManualMapper, find_signature
 
+import sys
+
 CURRENT_DIRECTORY = Path(__file__).parent
 
-if CURRENT_DIRECTORY.joinpath('license_server').exists():
-    license_server = ManualMapper('license_server')
-    license_server.load()
+LICENSE_SERVER_PATH = CURRENT_DIRECTORY.joinpath('license_server')
+VAULT_SERVER_PATH = CURRENT_DIRECTORY.joinpath('vault_server')
 
-    address = find_signature(
-        license_server.memory, license_server.image_size,
-        [ 0x64, 0x24, 0x76 ],
-        [ 0b11010000 ],
-        -0xd,
-        [
-            ( 0x3f, True, 0x5, 0x1, 0x4, 0x0 ),
-            ( 0x3b, True, 0x5, 0x1, 0x4, -0x1c )
-        ]
-    )
+def main():
 
-    if address:
-        print(f'Found address: {address:#x}')
-        license_server.memory[address:address + 6] = b'\x90\xE9\xCE\x00\x00\x00'
-        print('Patched!')
+    if LICENSE_SERVER_PATH.exists():
+        license_server = ManualMapper(LICENSE_SERVER_PATH)
+        license_server.load()
 
-    license_server.save(CURRENT_DIRECTORY.joinpath('license_server'))
-    license_server.unmap()
+        address = find_signature(
+            license_server.memory, license_server.image_size,
+            [ 0x64, 0x24, 0x76 ],
+            [ 0b11010000 ],
+            -0xd,
+            [
+                ( 0x3f, True, 0x5, 0x1, 0x4, 0x0 ),
+                ( 0x3b, True, 0x5, 0x1, 0x4, -0x1c )
+            ]
+        )
 
-if CURRENT_DIRECTORY.joinpath('vault_server').exists():
-    vault_server = ManualMapper('vault_server')
-    vault_server.load()
+        if address:
+            print(f'Found address: {address:#x}')
+            license_server.memory[address:address + 6] = b'\x90\xE9\xCE\x00\x00\x00'
+            print('Patched!')
 
-    address = find_signature(
-        vault_server.memory, vault_server.image_size,
-        [ 0x70, 0x0F, 0x8F ],
-        [ 0b10110000 ],
-        -0x14
-    )
+        license_server.save(LICENSE_SERVER_PATH)
+        license_server.unmap()
 
-    if address:
-        print(f'Found address: {address:#x}')
-        vault_server.memory[address:address + 6] = b'\x90' * 6
-        print('Patched!')
+    if VAULT_SERVER_PATH.exists():
+        vault_server = ManualMapper(VAULT_SERVER_PATH)
+        vault_server.load()
 
-    address = find_signature(
-        vault_server.memory, vault_server.image_size,
-        [ 0x70, 0x0F, 0x8F ],
-        [ 0b10110000 ],
-        0x2
-    )
+        address = find_signature(
+            vault_server.memory, vault_server.image_size,
+            [ 0x70, 0x0F, 0x8F ],
+            [ 0b10110000 ],
+            -0x14
+        )
 
-    if address:
-        print(f'Found address: {address:#x}')
-        vault_server.memory[address:address + 6] = b'\x90' * 6
-        print('Patched!')
+        if address:
+            print(f'Found address: {address:#x}')
+            vault_server.memory[address:address + 6] = b'\x90' * 6
+            print('Patched!')
 
-    address = find_signature(
-        vault_server.memory, vault_server.image_size,
-        [ 0xD2, 0x59, 0x5E ],
-        [ 0b11100000 ],
-        -0x37,
-        [
-            ( 0x0, True, 0x2, 0x1, 0x1, 0x0 ),
-            ( 0x3e, True, 0x6, 0x2, 0x4, 0x0 ),
-            ( -0xb, True, 0x5, 0x1, 0x4, -0x22 )
-        ]
-    )
+        address = find_signature(
+            vault_server.memory, vault_server.image_size,
+            [ 0x70, 0x0F, 0x8F ],
+            [ 0b10110000 ],
+            0x2
+        )
 
-    if address:
-        print(f'Found address: {address:#x}')
-        vault_server.memory[address:address + 6] = b'\x90\xE9\xE2\x01\x00\x00'
-        print('Patched!')
+        if address:
+            print(f'Found address: {address:#x}')
+            vault_server.memory[address:address + 6] = b'\x90' * 6
+            print('Patched!')
 
-    vault_server.save(CURRENT_DIRECTORY.joinpath('vault_server'))
-    vault_server.unmap()
+        address = find_signature(
+            vault_server.memory, vault_server.image_size,
+            [ 0xD2, 0x59, 0x5E ],
+            [ 0b11100000 ],
+            -0x37,
+            [
+                ( 0x0, True, 0x2, 0x1, 0x1, 0x0 ),
+                ( 0x3e, True, 0x6, 0x2, 0x4, 0x0 ),
+                ( -0xb, True, 0x5, 0x1, 0x4, -0x22 )
+            ]
+        )
+
+        if address:
+            print(f'Found address: {address:#x}')
+            vault_server.memory[address:address + 6] = b'\x90\xE9\xE2\x01\x00\x00'
+            print('Patched!')
+
+        vault_server.save(VAULT_SERVER_PATH)
+        vault_server.unmap()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
